941cbe122352343433f8bf01a6fbd63fe2c6e9fa
bartik369 <6apmuk@gmail.com>
2024-04-11 00:34:21 +0300

    2

--- src/App.tsx
+++ src/App.tsx
@@ -33,7 +33,6 @@ const App: FC = () => {
 
   return (
     <div className={style.wrapper}>
-      <div>Test runner one</div>
       <Header />
       <Routes>
         <Route path='/' element={<Home />} />
