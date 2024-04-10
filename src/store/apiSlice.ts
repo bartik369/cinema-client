3f96ffa111fac174fd74dbbc3fa71e0679f4d4a2
bartik369 <6apmuk@gmail.com>
2024-04-11 01:02:34 +0300

    9

--- src/store/apiSlice.ts
+++ src/store/apiSlice.ts
@@ -43,7 +43,7 @@ export const useValidateAccessToken = () => {
 
   const validateAccessToken = async(token:string) => {
       try {
-          const response = await axios.get(`${ENV.API_URL}api/verify-token/`, {
+          const response = await axios.get(`${ENV.API_URL}/verify-token/`, {
               withCredentials: true,
               headers: { Authorization: `Bearer ${token}`}
           });
