9477d7bdce7c0892bb518051eaeddfdfefbf27ad
bartik369 <6apmuk@gmail.com>
2024-04-15 19:13:27 +0300

    test 2

--- src/hooks/windowSizeHook.ts
+++ src/hooks/windowSizeHook.ts
@@ -12,8 +12,8 @@ export default function useWindowSizeHook() {
          setSlidesCount(6);
         }  else if (window.innerWidth > 650) {
          setSlidesCount(5);
-        } else if (window.innerWidth > 480) {
-            setSlidesCount(4);
+        } else if (window.innerWidth < 480) {
+            setSlidesCount(3);
            }
        }, [window.innerWidth])
        
