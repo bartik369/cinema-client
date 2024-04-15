43bb63f6cd9802a7f787ad800911bcd3b9d2ea4b
bartik369 <6apmuk@gmail.com>
2024-04-15 22:14:19 +0300

    check mobile version navbar

--- src/hooks/useCountTopHook.ts
+++ src/hooks/useCountTopHook.ts
@@ -10,11 +10,14 @@ export default function useCountTopHook() {
          setSlidesCount(7);
         } else if (window.innerWidth > 990) {
          setSlidesCount(6);
-        }  else if (window.innerWidth > 650) {
+        }  else if (window.innerWidth > 750) {
          setSlidesCount(5);
-        } else if (window.innerWidth > 480) {
-            setSlidesCount(4);
-        } else if (window.innerWidth < 480) {
+        } else if (window.innerWidth > 550) {
+            setSlidesCount(3);
+        } else if (window.innerWidth > 450) {
+            setSlidesCount(3);
+        }
+        else if (window.innerWidth > 350) {
             setSlidesCount(2);
         }
        }, [window.innerWidth])
