362193379aba6c09593193c6e2189aae7d696eab
bartik369 <6apmuk@gmail.com>
2024-05-01 15:37:27 +0300

    top slides fix

--- src/hooks/useCountTopHook.ts
+++ src/hooks/useCountTopHook.ts
@@ -5,13 +5,13 @@ export default function useCountTopHook() {
 
     useEffect(() => {
         if (window.innerWidth > 1200) {
-         setSlidesCount(8);
-        } else if (window.innerWidth > 1100) {
          setSlidesCount(7);
-        } else if (window.innerWidth > 990) {
+        } else if (window.innerWidth > 1100) {
          setSlidesCount(6);
-        }  else if (window.innerWidth > 750) {
+        } else if (window.innerWidth > 990) {
          setSlidesCount(5);
+        }  else if (window.innerWidth > 750) {
+         setSlidesCount(4);
         } else if (window.innerWidth > 550) {
             setSlidesCount(3);
         } else if (window.innerWidth > 450) {
