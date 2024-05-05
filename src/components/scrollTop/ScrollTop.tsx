8ffcdc9a8155057d5d143320f46fbedc4608406d
bartik369 <6apmuk@gmail.com>
2024-05-05 11:55:34 +0300

    1

--- src/components/scrollTop/ScrollTop.tsx
+++ src/components/scrollTop/ScrollTop.tsx
@@ -3,6 +3,7 @@ import { useLocation } from 'react-router-dom';
 
 const ScrollTop = () => {
     const {pathname} = useLocation();
+
     useEffect(() => {
         window.scrollTo(0, 0);
     }, [pathname]);
