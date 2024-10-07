487328010c7a717ee51f3616f1408ea5d5f73486
bartik369 <6apmuk@gmail.com>
2024-10-07 23:26:28 +0300

    test

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -11,7 +11,6 @@ import style from './Home.module.css';
 
 const Home: FC = () => {
   const [visibleInfo, setVisibleInfo] = useState<boolean>(false);
-
   useEffect(() => {
     if (localStorage.getItem('projectInformation') === 'true') {
       setVisibleInfo(false);
